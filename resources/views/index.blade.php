77bb63ec63654e6f2b717abba3eb12041d01777c
Valery Fedorenko <fedorenko@stranzit.ru>
2020-04-02 15:05:34 +0800

    Изменён текст приветствия

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,8 @@
 @section('content')
 <div class="container-fluid parallax parallax-main py-0 px-0">
     <div class="header-text py-5">
-        <h1 class="text-center py-5 my-5 display-1 text-primary">ООО Стоматология</h1>
-        <h3 class="text-center py-5 display-3 text-success">Ждём вас в гости, дорогие пациенты!</h3>
+        <h1 class="text-center py-5 my-5 display-1 text-primary">ООО "Стоматология"</h1>
+        <h3 class="text-center py-5 display-3 text-success">Мы работаем для вас!</h3>
     </div>
 </div>
 <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ad0a95e4dd7aef1dc64eefaf30c1cec34c87bbe1cc84da7a2a7763adf68b3507b&amp;source=constructor" width="100%" height="400" frameborder="0"></iframe>
