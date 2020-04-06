2b88cfe428fed89dd9a60724c8275dfb900d6d9d
Valery Fedorenko <fedorenko@stranzit.ru>
2020-04-06 09:18:59 +0800

    Исправлена вёрстка

--- resources/views/doctors.blade.php
+++ resources/views/doctors.blade.php
@@ -6,7 +6,7 @@
 <div class="container">
     <div class="card-columns">
         @foreach($doctors as $doctor)
-            <div class="card" style="width: 18rem;">
+            <div class="card bg-light">
                 <img class="card-img-top" src="storage/{{ $doctor->image }}" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title text-success">{{ $doctor->name }}</h5>
